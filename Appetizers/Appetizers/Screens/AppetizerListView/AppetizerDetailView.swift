4520a85c1455af2d186a5389ed6c325522b751e7
Carlos Kimura <ckimura@gmail.com>
2023-12-11 16:00:35 -0300

    passing data to detail view

--- Appetizers/Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
+++ Appetizers/Appetizers/Screens/AppetizerListView/AppetizerDetailView.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct AppetizerDetailView: View {
     
     let appetizer: Appetizer
+    @Binding var isShowingDetail: Bool
     
     var body: some View {
         VStack {
-            Image("asian-flank-steak")
-                .resizable()
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 320, height: 225)
             
@@ -86,7 +86,7 @@ struct AppetizerDetailView: View {
         .cornerRadius(12)
         .shadow(radius: 40)
         .overlay(Button {
-            print("dismiss")
+            isShowingDetail = false
         } label: {
             ZStack {
                 Circle()
@@ -104,5 +104,5 @@ struct AppetizerDetailView: View {
 }
 
 #Preview {
-    AppetizerDetailView(appetizer: MockData.sampleAppetizer)
+    AppetizerDetailView(appetizer: MockData.sampleAppetizer, isShowingDetail: .constant(true))
 }
