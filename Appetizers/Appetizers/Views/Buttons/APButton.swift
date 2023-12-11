8eff4581d1168ba322d41e7e9099a4571021917e
Carlos Kimura <ckimura@gmail.com>
2023-12-11 16:28:37 -0300

    appetizer detail view refactor

---
+++ Appetizers/Appetizers/Views/Buttons/APButton.swift
@@ -0,0 +1,27 @@
+//
+//  APButton.swift
+//  Appetizers
+//
+//  Created by Carlos Kimura on 11/12/23.
+//
+
+import SwiftUI
+
+struct APButton: View {
+    
+    let title: LocalizedStringKey
+    
+    var body: some View {
+        Text(title)
+            .font(.title3)
+            .fontWeight(.semibold)
+            .frame(width: 260, height: 50)
+            .foregroundColor(.white)
+            .background(.brandPrimary)
+            .cornerRadius(10)
+    }
+}
+
+#Preview {
+    APButton(title: "Appetizer")
+}
