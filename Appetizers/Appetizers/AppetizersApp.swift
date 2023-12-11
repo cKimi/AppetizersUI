f6901dc736447a5e135c38eb0cc73af7077b272b
Carlos Kimura <ckimura@gmail.com>
2023-12-11 10:50:57 -0300

    initial project files

---
+++ Appetizers/Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Carlos Kimura on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
